907ecd7842b17b0fe9171f894460bde77d0741a5
NguyenHuuChi <22chi.nh@vinuni.edu.vn>
2025-08-01 17:23:36 +0200

    integrate quantization into strategy

--- scripts/eval_hq44k.sh
+++ scripts/eval_hq44k.sh
@@ -6,4 +6,4 @@
 #     --eval \
 #     --restore-model ./work_dirs/hq_sam_vit_b/epoch_11.pth
 
-CUDA_VISIBLE_DEVICES=0,1,2 torchrun --nproc_per_node=1 quant/hq44k_engine.py
+CUDA_VISIBLE_DEVICES=0,1,2 torchrun --nproc_per_node=1 quant/hq44k_engine_quan.py
