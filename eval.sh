b2e258e4bf2ef96cd96c448550cb44531472e2cd
Chau Tran <chauht2@blender10.cs.illinois.edu>
2025-10-03 21:25:32 -0500

    updaet

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-python small_engine.py --mode eval  --quantize-decoder --n-bits 4 --num-samples 400 --num-calib-samples 16 
+python small_engine.py --mode eval  --quantize-encoder --n-bits 8 --num-samples 400 --num-calib-samples 16 
