ad329a33d3de7995dc4245a0ecdb7e31ff07d75b
Chau <tranhoaichau.00@gmail.com>
2026-04-25 15:39:20 +0000

    update

--- eval_tome.sh
+++ eval_tome.sh
@@ -1,6 +1,6 @@
 python benchmark_tome.py     \
-    --algos  sparsesam_random sparsesam\
-    --ratios 0.1  --sparsity 0.25 \
+    --algos  sparsesam sparsesam_random\
+    --ratios 0.75 0.5  0.25 0.1 0.05 --sparsity 0.25 \
     --batch-sizes 2 \
     --model-ckt ./ckts/sam_hq_vit_l.pth  --model-type vit_l    \
     --num-samples 100 --no-wandb
